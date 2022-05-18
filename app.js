95857805f1e3532a1a3cf636d9a97fa160480ee2
unknown <muskaan2lamba@gmail.com>
2022-05-19 00:18:45 +0530

    Learnt expressjs and different functionality it provides

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const http = require('http');
-const routes = require('./routes')
+// const http = require('http');
 
-console.log(routes.someText)
-const server = http.createServer(routes.handler)
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-server.listen(3000);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','404-page.html'))
+})
+
+app.listen(3000);
+// const server = http.createServer(app)
+
+// server.listen(3000);
