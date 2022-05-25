f61baabb4c94f95b558003e7f5112e5db7daf06f
unknown <muskaan2lamba@gmail.com>
2022-05-26 00:14:14 +0530

    Learnt various templating engines

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,16 @@ const express = require('express');
 const path = require('path');
 
 const router = express.Router();
+
 const rootDir = require('../util/path')
+const adminData = require('./admin');
 
 router.get('/',(req,res,next)=>{
-    console.log('Welcome to express');
-    res.sendFile(path.join(rootDir,'views', 'shop.html'));
+    // console.log('Welcome to express');
+    // console.log(adminData.products)
+    // res.sendFile(path.join(rootDir,'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true});
 })
 
 module.exports = router;
